3313a2929737eb5ffa9013e449a545efbae1940b
Kim Prado <kimprado21@gmail.com>
2020-01-28 17:30:40 -0300

    TestConsultarAtivos

--- testes-integracao-concorrente/internal/pkg/aluguel/equipamento_it_test.go
+++ testes-integracao-concorrente/internal/pkg/aluguel/equipamento_it_test.go
@@ -40,3 +40,31 @@ func TestConsultarMartelo(t *testing.T) {
 	assert.Equal(t, "e-001", v.Codigo)
 
 }
+
+func TestConsultarAtivos(t *testing.T) {
+	t.Parallel()
+
+	var err error
+	c, err := initializeConfigTest()
+	if err != nil {
+		t.Errorf("Erro ao criar Configuração: %+v\n", err)
+		return
+	}
+
+	c.MongoDB.Database = c.MongoDB.Database + "-" + t.Name()
+
+	p, err := initializeEquipamentoRepositoryTest(c)
+	if err != nil {
+		t.Errorf("Erro ao criar Repository: %+v\n", err)
+		return
+	}
+
+	assert.NotNil(t, p)
+
+	lista, err := p.Ativos()
+	assert.Nil(t, err)
+	assert.NotNil(t, lista)
+
+	assert.Equal(t, 2, len(lista))
+
+}
